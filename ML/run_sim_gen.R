37416309173ff4ae54bd6d2401470bd8e502605c
hemstrow <whemstrom@ucdavis.edu>
2020-07-07 21:37:07 -0700

    updated run_sim_gen with sim_gen updates

--- ML/run_sim_gen.R
+++ ML/run_sim_gen.R
@@ -2,29 +2,26 @@ library(GeneArchEst)
 #=========generate a bunch of sims============
 # ABC params
 ## priors
-h_func <- function(x) rep(.75, x)
 df_func <- function(x) runif(x, 1, 100)
-
 
 args <- commandArgs(TRUE)
 x <- as.character(args[1])
 y <- as.character(args[2])
 outname <- as.character(args[3])
-sim_iter <- as.character(args[4])
+par <- 24
+iters <- 100000
 
 x <- readRDS(x)
 meta <- x$meta
-x <- x$x
+x <- x[[3]]
 
-res <- readRDS(y)
+res <- readRDS(y)$ABC_res
 
-sims <- sim_gen(x = x, meta = meta, iters = 1, chr = "group", center = T,
-                pi_func = "joint", df_func = df_func,
-                scale_func = "joint", h_func = h_func, joint_res = res,
-                par = F, joint_res_dist = "ks", joint_acceptance = .05)
+sims <- sim_gen(x = x, meta = meta, iters = iters, center = T, scheme = "gwas", 
+                parameter_distributions = list(pi = "joint", scale = "joint", d.f = df_func), 
+                h_dist = function(x) rnorm(x, .5, .1), par = par, joint_res = res, joint_acceptance = 0.005, 
+                joint_res_dist = "ks")
 
-sims$stats$iter <- sim_iter
-write.table(sims$stats, paste0("stats_", outname), sep = "\t", quote = F, col.names = F, row.names = F)
-write.table(sims$effects, paste0("effects_", sim_iter, "_", outname), sep = "\t", quote = F, col.names = F, row.names = F)
+data.table::fwrite(sims$stats, paste0("stats_", outname), sep = "\t", quote = F, col.names = T, row.names = F)
 
 
