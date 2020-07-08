49d2da929f4714c05b0ff902864f9e212ccd4774
hemstrow <whemstrom@ucdavis.edu>
2020-07-07 21:57:30 -0700

    edits to run_sim_gen

--- ML/run_sim_gen.sh
+++ ML/run_sim_gen.sh
@@ -1,17 +1,15 @@
 #!/bin/bash -l
-#SBATCH --array=1-10000
-#SBATCH --mem=12G
-#SBATCH -t 2-24:00:00
+#SBATCH -t 7-24:00:00
 #SBATCH -J esd_sim_test
+#SBATCH --mem=60G
+#SBATCH -t 4-24:00:00
+#SBATCH -n 24
 
 
-infile=~/coalescence/prediction_accuracy/genomic_prediction_accuracy/ABC/ABC_input_fixed_number_flat_100_sites_scale_1.RDS
-outname=fixed_number_flat_100_sites_scale_1_sim_gen
-scheme_D_res=~/coalescence/prediction_accuracy/genomic_prediction_accuracy/ABC/fixed_flat_100sites_1scale/ABC_scheme_D_dual_opt_res_flat_scale1_sites100.RDS
+infile=~/coalescence/prediction_accuracy/genomic_prediction_accuracy/ABC/ABC_input_scale_1_pi_9999_h_5_df_5.RDS
+outname=ABC_input_scale_1_pi_9999_h_5_df_5.RDS
+scheme_D_res=~/coalescence/prediction_accuracy/genomic_prediction_accuracy/ABC/pi_9999_scale_1_h_5_df_5/ABC_scheme_D_.RDS
 
 module load gcc/9.2.0
 
-mkdir r_${SLURM_ARRAY_TASK_ID}
-cd r_${SLURM_ARRAY_TASK_ID}
-
-Rscript ~/coalescence/prediction_accuracy/genomic_prediction_accuracy/ML/run_sim_gen.R $infile $scheme_D_res $outname $SLURM_ARRAY_TASK_ID
+Rscript ~/coalescence/prediction_accuracy/genomic_prediction_accuracy/ML/run_sim_gen.R $infile $scheme_D_res $outname
