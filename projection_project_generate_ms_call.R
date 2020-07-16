95fb9380f80d0736623fd32f8f5009803e96d87e
hemstrow <whemstrom@ucdavis.edu>
2020-07-16 01:41:15 -0700

    fixed ms call

--- projection_project_generate_ms_call.R
+++ projection_project_generate_ms_call.R
@@ -1,9 +1,9 @@
 chrsize <- 10000000
 mu <- 1e-8
-Ne <- 10000
+Ne <- 1000
 r <- 2*1e-8
 
-gc <- 1000
+gc <- 2000
 chrnum <- 10
 
 theta <- 4*Ne*mu*chrsize
@@ -12,6 +12,6 @@ rho <- 4*Ne*r*(chrsize - 1)
 
 options(scipen = 999)
 
-call <- paste0("ms ", gc, " ", chrnum, " -t ", theta, " -r ", rho, " ", chrsize)
+call <- paste0("scrm ", gc, " ", chrnum, " -t ", theta, " -r ", rho, " ", chrsize)
 call
 
