9b6c0946c4e1522371240bcaafac56e076be8bce
hemstrow <whemstrom@ucdavis.edu>
2020-07-16 01:23:58 -0700

    first clean-up commit

--- find_big_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#finds files that are larger than 50mb and adds them to the .gitignore
-
-find . -size +50M -exec ls {} \+ | grep -v ".git/" - | cat - .gitignore | sort | uniq > out
-rm .gitignore
-mv out .gitignore
