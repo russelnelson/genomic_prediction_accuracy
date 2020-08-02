3522e354844b78d79a0eb9a3e66fe831e76df287
hemstrow <whemstrom@ucdavis.edu>
2020-08-02 13:38:41 -0700

    fixing the populus data prep to work with the big data

--- scripts/prep_populus_data.R
+++ scripts/prep_populus_data.R
@@ -1,11 +1,13 @@
 # read and prep the populus data
 ## read
 library(GeneArchEst)
-dat <- process_vcf("~/coalescence/prediction_accuracy/genomic_prediction_accuracy/data/populus_data/gatk_882_WG_genotypes_biallelic_snps_VQSR_non_filtered_bis.gt.vcf.gz")
-phenos <- data.table::fread("~/coalescence/prediction_accuracy/genomic_prediction_accuracy/data/populus_data/sorted_phenotypes.txt")
-meta <- dat$meta
+args <- commandArgs(TRUE)
+dat <- as.character(args[1])
+phenos <- as.character(args[2])
+
+# read in phenotypes, figure out which have data for our phenotype
+phenos <- data.table::fread(phenos)
+
+
 
 ## clean and save
-dat <- clean_phenotypes(dat$genotypes, phenos$`Stomatal density (SD)`)
-saveRDS(list(full = FALSE, missing = FALSE, imputed = dat$genotypes, meta = meta, phenos = list(p = dat$phenos)),
-        "~/coalescence/prediction_accuracy/genomic_prediction_accuracy/results/populus/geno_pheno.RDS")
--- scripts/run_prep_populus_data.sh
+++ scripts/run_prep_populus_data.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -l
-#SBATCH --mem=128G
+#SBATCH --mem=200G
 #SBATCH -t 4-24:00:00
 #SBATCH -J prep_populus
 #SBATCH -p bigmemm
 
-Rscript ~/coalescence/prediction_accuracy/genomic_prediction_accuracy/scripts/prep_populus_data.R
+data=~/coalescence/prediction_accuracy/genomic_prediction_accuracy/data/populus_data/gatk_882_WG_genotypes_biallelic_snps_VQSR_non_filtered_bis.gt.vcf
+
+sed -i '/0\\|1/ s//1/g' $data
+sed -i '/0\\|0/ s//0/g' $data
+sed -i '/1\\|1/ s//2/g' $data
+sed -i '/1\\|0/ s//1/g' $data
+
+Rscript ~/coalescence/prediction_accuracy/genomic_prediction_accuracy/scripts/prep_populus_data.R $data
