9f33dd0b6c836eb120b7b03ce58a387d032cddff
hemstrow <whemstrom@ucdavis.edu>
2020-08-23 17:53:00 -0700

    added a script to subset the populus snps for gmmat and g matrix creation.

---
+++ scripts/get_subset_snps.R
@@ -0,0 +1,17 @@
+library(bigstatsr)
+# import data
+dat <- big_attach(rdsfile = "/home/hemstrow/coalescence/prediction_accuracy/genomic_prediction_accuracy/data/populus_data/gatk_882_WG_genotypes_biallelic_snps_VQSR_non_filtered_bis.gt.rds")
+
+# find mafs
+rs <- big_apply(dat, a.FUN = function(X, ind) rowSums(X[, ind]),a.combine ='plus')
+maf <- rs/(2*ncol(dat))
+too.big <- which(maf > 0.5)
+maf[too.big] <- 1 - maf[too.big]
+
+# get snps to keep
+good <- which(maf >= 0.05)
+sub_snps <- sort(sample(good, 50))
+
+# save
+write.table(sub_snps, "/home/hemstrow/coalescence/prediction_accuracy/genomic_prediction_accuracy/data/populus_data/subset_snps.txt", row.names = F, col.names = F, quote = F)
+
